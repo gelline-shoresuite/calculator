d76db551513d89c78d3fef31cb20a4833d171ec4
Gelline Gruta <gelline.gruta@shoresuite.com>
2019-02-07 09:33:23 +0800

    Added build in docs

---
+++ docs/precache-manifest.c9410fd29bf0c60ea668596ee5978e7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2efcc5e1cc5a89948d32",
+    "url": "/static/css/main.e2d263f7.chunk.css"
+  },
+  {
+    "revision": "2efcc5e1cc5a89948d32",
+    "url": "/static/js/main.2efcc5e1.chunk.js"
+  },
+  {
+    "revision": "d35ebfcd92035b029a34",
+    "url": "/static/js/1.d35ebfcd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ddb34991b4bc38dbd8e61baf1438a64a",
+    "url": "/index.html"
+  }
+];
